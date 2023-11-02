c2a9ba62749ca10e8acd403df96cd89bbabe7b84
Yakin Nikita <yakin95@gmail.com>
2023-11-02 13:15:55 +0500

    chore: remove auto import

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+	components: false,
+	imports: {
+		autoImport: false
+	},
 	devtools: { enabled: true },
 	css: ['assets/styles/index.scss'],
 	modules: [
