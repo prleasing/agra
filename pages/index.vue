5043df4a34d767801a3586542a105336160f8dce
vadimedu <paketbananov@MacBook-Pro-Paket.local>
2023-11-02 11:29:24 +0500

    feat: index add vacancy component on the main page
     add vacancy component on the main page

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,10 @@
 !
 <template>
-	<div>Главная</div>
+	<div>
+		<the-vacancy />
+	</div>
 </template>
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+// import TheVacansy from '../components/the-vacancy.vue';
+</script>
 <!-- <style lang="scss"></style> -->
