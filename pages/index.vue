b19047aa362ce119c5d5499989eeb074cb3a67b5
vadimedu <paketbananov@MacBook-Pro-Paket.local>
2023-11-21 12:37:11 +0500

    feat:index change props to contact-form section
    change props to contact-form section

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,33 @@
-!
 <template>
 	<the-page>
+		<section-grow-farm />
+		<section-directions />
+		<section-farming />
 		<section-growing />
 		<section-vacancy />
 		<section-about />
-		<section-contact-form />
+		<section-contact-form id="contact-form" name="contact-form" />
 	</the-page>
 </template>
 <script lang="ts" setup>
+import { definePageMeta } from '#imports';
 import SectionAbout from '~/components/landing/section-about';
 import SectionContactForm from '~/components/landing/section-contact-form';
+import SectionDirections from '~/components/landing/section-directions';
+import SectionFarming from '~/components/landing/section-farming';
+import SectionGrowFarm from '~/components/landing/section-grow-farm';
 import sectionGrowing from '~/components/landing/section-growing';
 import SectionVacancy from '~/components/landing/section-vacancy';
 import ThePage from '~/components/the-page';
+import { Pages } from '~/utils/pages';
+
+definePageMeta({
+	name: Pages.Home
+});
 </script>
-<!-- <style lang="scss"></style> -->
+<style scoped lang="scss">
+@use 'assets/styles/utility';
+.section {
+	display: grid;
+}
+</style>
