67353f3e8932a8f985ddb309ae34129ba73a5ab5
vadimedu <paketbananov@MacBook-Pro-Paket.local>
2023-11-23 12:20:49 +0500

    fix:header change links in header
    change links in header

--- components/core/the-header.vue
+++ components/core/the-header.vue
@@ -1,23 +1,23 @@
 <template>
-	<header>
-		<div class="header">
-			<div class="header__logo-wrapper">
-				<base-logo />
-				<nuxt-link to="/"></nuxt-link>
+	<header class="header">
+		<div class="header__logo-wrapper">
+			<base-logo />
+			<nuxt-link to="/">
+				<sr-only>На главную</sr-only>
+			</nuxt-link>
+		</div>
+		<div class="header__items-wrapper">
+			<div v-for="item in menu" :key="item.id" class="header__menu-item">
+				<nuxt-link :to="`/#${item.link}`">{{ item.title }}</nuxt-link>
 			</div>
-			<div class="header__items-wrapper">
-				<div v-for="item in menu" :key="item.id" class="header__menu-item">
-					{{ item.title }}
-				</div>
-				<div class="header__cart">
-					<nuxt-link :to="{ path: Pages.Basket }">
-						<base-icon :icon="Icons24ShoppingCart" />
-					</nuxt-link>
-					<div v-show="showInfo" class="header__cart-info">
-						<client-only
-							><span>{{ length }}</span></client-only
-						>
-					</div>
+			<div class="header__cart">
+				<nuxt-link :to="{ path: Pages.Basket }">
+					<base-icon :icon="Icons24ShoppingCart" />
+				</nuxt-link>
+				<div v-show="showInfo" class="header__cart-info">
+					<client-only
+						><span>{{ length }}</span></client-only
+					>
 				</div>
 			</div>
 		</div>
@@ -30,16 +30,17 @@ import { Icons24ShoppingCart } from '#icons';
 import { computed, ref } from '#imports';
 import BaseIcon from '~/components/elements/base-icon.vue';
 import BaseLogo from '~/components/elements/base-logo.vue';
+import srOnly from '~/components/utils/srOnly.vue';
 import { useStoreBasket } from '~/store/storeBasket';
 import { Pages } from '~/utils/pages';
 
 const basket = useStoreBasket();
 const menu = ref([
-	{ id: 1, title: 'Вакансии' },
-	{ id: 2, title: 'О Компании' },
-	{ id: 3, title: 'Направление деятельности' },
-	{ id: 4, title: 'Cобственная продукция' },
-	{ id: 5, title: 'Контакты' }
+	{ id: 1, title: 'Вакансии', link: 'vacancy' },
+	{ id: 2, title: 'О Компании', link: 'about-company' },
+	{ id: 3, title: 'Направление деятельности', link: 'directions' },
+	{ id: 4, title: 'Cобственная продукция', link: 'products' },
+	{ id: 5, title: 'Контакты', link: 'contact-form' }
 ]);
 const showInfo = computed(() => {
 	if (Object.keys(basket.items).length) {
@@ -54,84 +55,101 @@ const length = computed(() => {
 <style scoped lang="scss">
 @use 'assets/styles/utility';
 
-header {
+.header {
+	margin: 0 auto;
 	display: flex;
-	justify-content: center;
-
-	.header {
+	justify-content: space-between;
+	align-items: center;
+	width: var(--container);
+	// height: #{utility.rem(97)};
+	background-color: #fff;
+	border-radius: #{utility.rem(16)};
+	padding: #{utility.rem(8)};
+	position: sticky;
+	z-index: 3;
+	top: #{utility.rem(16)};
+	&__items-wrapper {
 		display: flex;
-		justify-content: space-between;
+	}
+
+	&__logo-wrapper {
+		position: relative;
+
+		> a {
+			position: absolute;
+			top: 0;
+			left: 0;
+			width: 100%;
+			height: 100%;
+		}
+	}
+
+	&__menu-item {
+		padding: #{utility.rem(16)};
+		color: #1d2939;
+		font-weight: 500;
+		font-style: normal;
+		font-size: #{utility.rem(18)};
+		line-height: 135%;
+		letter-spacing: #{utility.rem(-0.54)};
+		a {
+			text-decoration: none;
+		}
+	}
+	&__menu-item:hover a {
+		text-decoration: underline;
+	}
+
+	&__cart {
+		position: relative;
+		display: flex;
+		justify-content: center;
 		align-items: center;
-		width: var(--container);
-		// height: #{utility.rem(97)};
+		width: #{utility.rem(56)};
+		height: #{utility.rem(56)};
+		margin-left: #{utility.rem(16)};
+		border-radius: #{utility.rem(100)};
 		background-color: #fff;
-		border-radius: #{utility.rem(16)};
-		padding: #{utility.rem(8)};
-		position: fixed;
-		z-index: 3;
-		&__items-wrapper {
-			display: flex;
+		border: 1px solid #1d2939;
+		transition: background-color 0.2s ease-in-out;
+		.icon {
+			width: #{utility.rem(24)};
+			transition: background-color 0.2s ease-in-out;
 		}
-
-		&__logo-wrapper {
-			position: relative;
-
-			> a {
-				position: absolute;
-				top: 0;
-				left: 0;
-				width: 100%;
-				height: 100%;
-			}
-		}
-
-		&__menu-item {
-			padding: #{utility.rem(16)};
-			color: #1d2939;
-			font-weight: 500;
-			font-style: normal;
-			font-size: #{utility.rem(18)};
-			line-height: 135%;
-			letter-spacing: #{utility.rem(-0.54)};
-		}
-
-		&__cart {
-			position: relative;
+		> a {
+			position: absolute;
+			width: 100%;
+			height: 100%;
 			display: flex;
 			justify-content: center;
-			align-items: center;
-			width: #{utility.rem(56)};
-			height: #{utility.rem(56)};
-			margin-left: #{utility.rem(16)};
-			border-radius: #{utility.rem(100)};
-			background-color: #fff;
-			border: 1px solid #1d2939;
-			.icon {
-				width: #{utility.rem(24)};
-			}
 		}
+	}
+	&__cart:hover .icon {
+		color: #fff;
+	}
+	&__cart:hover {
+		background-color: #1D2939;
+	}
+	&__cart-info {
+		position: absolute;
+		top: #{utility.rem(-3)};
+		right: 0;
+		display: inline;
+		width: #{utility.rem(24)};
+		height: #{utility.rem(24)};
+		margin: 0;
+		padding: 0;
+		border-radius: #{utility.rem(100)};
+		background-color: var(--brand);
+		text-align: center;
 
-		&__cart-info {
-			position: absolute;
-			top: #{utility.rem(-3)};
-			right: 0;
-			display: inline;
-			width: #{utility.rem(24)};
-			height: #{utility.rem(24)};
-			margin: 0;
-			padding: 0;
-			border-radius: #{utility.rem(100)};
-			background-color: var(--brand);
-			text-align: center;
-
-			> span {
-				width: 100%;
-				margin: 0 auto;
-				color: #fff;
-				font-weight: 600;
-				font-style: normal;
-				font-size: #{utility.rem(14)};
-			}
+		> span {
+			width: 100%;
+			margin: 0 auto;
+			color: #fff;
+			font-weight: 600;
+			font-style: normal;
+			font-size: #{utility.rem(14)};
 		}
 	}
 }
