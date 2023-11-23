29ed99f10803301f51fddb01d85d67f4c932eefb
vadimedu <paketbananov@MacBook-Pro-Paket.local>
2023-11-23 12:00:45 +0500

    feat:srOnly add srOnly universal component
    add srOnly universal component

---
+++ components/utils/srOnly.vue
@@ -0,0 +1,25 @@
+<template>
+	<component :is="tag" class="sr-only">
+		<slot />
+	</component>
+</template>
+
+<script setup lang="ts">
+import type { PropType } from 'vue';
+
+defineProps({
+	tag: {
+		// eslint-disable-next-line no-undef
+		type: String as PropType<keyof HTMLElementTagNameMap>,
+		default: 'span'
+	}
+});
+</script>
+
+<style lang="scss" scoped>
+@use 'assets/styles/utility';
+
+.sr-only {
+	@include utility.sr-only;
+}
+</style>
