63e6f9e775ee2cc72b0851fa2f22f1376c8dbe20
vadimedu <paketbananov@MacBook-Pro-Paket.local>
2023-11-27 09:11:03 +0500

    feat:base-section add mobile styles
    add mobile styles

--- components/base-section.vue
+++ components/base-section.vue
@@ -20,18 +20,29 @@ defineProps({
 </script>
 <style scoped lang="scss">
 @use 'assets/styles/utility';
+@use 'assets/styles/breakpoints';
 
 .section {
 	display: grid;
 	gap: #{utility.rem(24)};
 	width: var(--container);
 	margin: 0 auto;
+	@include breakpoints.media-down('xl') {
+		width: math(var(--mobile-container));
+		max-width: var(--mobile-container-max-width);
+		padding: var(--ears);
+	}
 
 	&__content {
 		margin: 0 #{utility.rem(-24)};
 		padding: #{utility.rem(24)};
 		border-radius: #{utility.rem(32)};
 		background: #fff;
+		@include breakpoints.media-down('xl') {
+			margin: 0 #{utility.rem(-16)};
+			padding: #{utility.rem(16)};
+			border-radius: #{utility.rem(24)};
+		}
 	}
 
 	& &__title {
@@ -41,6 +52,15 @@ defineProps({
 		font-size: #{utility.rem(48)};
 		line-height: 115%;
 		letter-spacing: #{utility.rem(-0.96)};
+		@include breakpoints.media-down('xl') {
+			font-size: #{utility.rem(24)};
+			font-style: normal;
+			font-weight: 700;
+			line-height: 115%;
+			letter-spacing: #{utility.rem(-0.96)};
+			display: flex;
+			flex-wrap: wrap;
+		}
 	}
 }
 </style>
