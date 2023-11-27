ff7bb54a2f6e83a6d1ebaa87ed4b458c26317403
vadimedu <paketbananov@MacBook-Pro-Paket.local>
2023-11-27 09:07:34 +0500

    feat:the-page add gap for mobile
    add gap for mobile

--- components/the-page.vue
+++ components/the-page.vue
@@ -8,9 +8,13 @@
 
 <style scoped lang="scss">
 @use 'assets/styles/utility';
+@use 'assets/styles/breakpoints';
 
 .page {
 	display: grid;
 	gap: #{utility.rem(150)};
+	@include breakpoints.media-down('xl') {
+		gap: #{utility.rem(64)};
+	}
 }
 </style>
