44ad60892b29e7a803e2bab21f2e3c00956d6bcd
vadimedu <paketbananov@MacBook-Pro-Paket.local>
2023-11-27 09:09:25 +0500

    feat: grow-farm add mobile styles
    add mobile styles

--- components/landing/section-grow-farm.vue
+++ components/landing/section-grow-farm.vue
@@ -13,7 +13,22 @@
 			</div>
 		</div>
 		<div class="grow-farm__picture">
-			<base-picture :width="1920" :height="707" alt="grow-photo" src="/images/landing/home/hay-cow.png"></base-picture>
+			<div class="grow-farm__picture-desktop">
+				<base-picture
+					:width="1920"
+					:height="707"
+					alt="grow-photo"
+					src="/images/landing/home/hay-cow.png"
+				></base-picture>
+			</div>
+			<div class="grow-farm__picture-mobile">
+				<base-picture
+					:width="320"
+					:height="320"
+					alt="grow-photo"
+					src="/images/landing/home/hay-cow-mobile.png"
+				></base-picture>
+			</div>
 		</div>
 	</div>
 </template>
@@ -25,12 +40,35 @@ const target = ref();
 </script>
 <style scoped lang="scss">
 @use 'assets/styles/utility';
+@use 'assets/styles/breakpoints';
+
 .grow-farm {
 	width: var(--container);
 	display: flex;
 	justify-self: center;
 	margin-top: #{utility.rem(164)};
 	position: relative;
+	@include breakpoints.media-down('xl') {
+		width: var(--mobile-container);
+		max-width: var(--mobile-container-max-width);
+		flex-direction: column;
+		align-items: center;
+		padding: var(--ears);
+		margin-top: unset;
+		justify-self: center;
+	}
+	&__picture-mobile {
+		display: none;
+		@include breakpoints.media-down('xl') {
+			display: block;
+			width: 100%;
+		}
+	}
+	&__picture-desktop {
+		@include breakpoints.media-down('xl') {
+			display: none;
+		}
+	}
 	&__title {
 		color: #1d2939;
 		font-size: #{utility.rem(48)};
@@ -40,6 +78,13 @@ const target = ref();
 		letter-spacing: #{utility.rem(-1.92)};
 		max-width: #{utility.rem(575)};
 		display: inline-block;
+		@include breakpoints.media-down('xl') {
+			font-size: #{utility.rem(24)};
+			font-style: normal;
+			font-weight: 700;
+			line-height: 115%;
+			letter-spacing: #{utility.rem(-0.96)};
+		}
 		.grow-farm__title-green {
 			padding-left: #{utility.rem(12)};
 			padding-right: #{utility.rem(12)};
@@ -52,10 +97,19 @@ const target = ref();
 		font-weight: 700;
 		line-height: 115%;
 		letter-spacing: #{utility.rem(-1.92)};
+		@include breakpoints.media-down('xl') {
+			font-size: #{utility.rem(24)};
+			font-style: normal;
+			font-weight: 700;
+			line-height: 115%;
+			letter-spacing: #{utility.rem(-0.96)};
+		}
 	}
 	&__title-container {
 		display: flex;
 		flex-direction: column;
+		width: 100%;
+		order: 2;
 	}
 	&__picture {
 		position: absolute;
@@ -63,6 +117,20 @@ const target = ref();
 		left: #{utility.rem(-380)};
 		z-index: -1;
 		width: 137%;
+		@include breakpoints.media-down('xl') {
+			position: relative;
+			top: unset;
+			left: unset;
+			width: 100%;
+			max-width: 100%;
+			justify-content: center;
+			align-items: center;
+			display: flex;
+			order: 1;
+			margin-left: #{utility.rem(-16)};
+			margin-right: #{utility.rem(-16)};
+			// width: 100%;
+		}
 	}
 	&__call {
 		padding: #{utility.rem(24)} #{utility.rem(32)} #{utility.rem(24)} #{utility.rem(32)};
@@ -85,6 +153,16 @@ const target = ref();
 			0px #{utility.rem(42)} #{utility.rem(25)} 0px rgba(29, 41, 57, 0.04),
 			0px #{utility.rem(74)} #{utility.rem(30)} 0px rgba(29, 41, 57, 0.01),
 			0px #{utility.rem(116)} #{utility.rem(33)} 0px rgba(29, 41, 57, 0);
+		@include breakpoints.media-down('xl') {
+			padding: #{utility.rem(16)} #{utility.rem(24)} #{utility.rem(16)} #{utility.rem(24)};
+			max-width: #{utility.rem(190)};
+			font-size: 18px;
+			font-style: normal;
+			font-weight: 500;
+			line-height: 135%;
+			letter-spacing: -1.08px;
+			border-radius: #{utility.rem(12)};
+		}
 		a {
 			text-decoration: none;
 		}
@@ -109,6 +187,22 @@ const target = ref();
 		font-weight: 500;
 		line-height: 135%;
 		letter-spacing: #{utility.rem(-0.48)};
+		@include breakpoints.media-down('xl') {
+			color: rgba(29, 41, 57, 0.80);
+			font-size: #{utility.rem(18)};
+			font-style: normal;
+			font-weight: 500;
+			line-height: 135%;
+			letter-spacing: #{utility.rem(-0.36)};
+		}
+	}
+}
+.picture {
+	@include breakpoints.media-down('xl') {
+		margin-left: #{utility.rem(-16)};
+		margin-right: #{utility.rem(-16)};
+		display: flex;
+		justify-content: center;
 	}
 }
 </style>
