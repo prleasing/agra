7ff7b158d2c2a7d61cf152daa8cb3dca098b65da
vadimedu <paketbananov@MacBook-Pro-Paket.local>
2023-11-29 12:15:07 +0500

    feat:the-cultures add mobile styles
    add mobile styles

--- components/landing/the-cultures.vue
+++ components/landing/the-cultures.vue
@@ -143,11 +143,20 @@ const productStore = useStoreProduct();
 </script>
 <style scoped lang="scss">
 @use 'assets/styles/utility';
+@use 'assets/styles/breakpoints';
 
 .cultures {
 	display: grid;
 	grid-template-columns: repeat(3, 1fr);
 	gap: #{utility.rem(16)};
 	aspect-ratio: 1/1;
+	@include breakpoints.media-down('xl') {
+		grid-template-columns: 1fr 1fr;
+		// grid-template-columns: repeat(1, 1fr);
+	}
+	@include breakpoints.media-down('mmd') {
+		grid-template-columns: 1fr;
+		// grid-template-columns: repeat(1, 1fr);
+	}
 }
 </style>
