c0679a220f2759ddce5c812f7c36a6c0466c5f36
vadimedu <paketbananov@MacBook-Pro-Paket.local>
2023-12-01 10:26:03 +0500

    feat:header change header to adapt to mobile version
    change header to adapt to mobile version

--- components/core/the-header.vue
+++ components/core/the-header.vue
@@ -1,60 +1,90 @@
 <template>
-	<header class="header">
-		<div class="header__logo-wrapper">
-			<base-logo />
-			<nuxt-link to="/">
-				<sr-only>На главную</sr-only>
-			</nuxt-link>
-		</div>
-		<div class="header__items-wrapper">
-			<div v-for="item in menu" :key="item.id" class="header__menu-item">
-				<nuxt-link :to="`/#${item.link}`">{{ item.title }}</nuxt-link>
+	<div>
+		<header class="header">
+			<div class="header__logo-wrapper">
+				<base-logo />
+				<nuxt-link to="/">
+					<sr-only>На главную</sr-only>
+				</nuxt-link>
 			</div>
-			<div class="header__menu-cart-wrapper">
-				<div class="header__cart">
-					<nuxt-link :to="{ path: Pages.Basket }">
-						<base-icon :icon="Icons24ShoppingCart" />
-					</nuxt-link>
-					<div v-show="showInfo" class="header__cart-info">
-						<client-only
-							><span>{{ length }}</span></client-only
-						>
+			<div class="header__items-wrapper">
+				<menu-items></menu-items>
+				<!-- <div v-for="item in menu" :key="item.id" class="header__menu-item">
+					<nuxt-link :to="`/#${item.link}`">{{ item.title }}</nuxt-link>
+				</div> -->
+				<!-- <contacts :phone="phone" :email="email"></contacts> -->
+				<div class="header__menu-cart-wrapper">
+					<div class="header__cart">
+						<nuxt-link :to="{ path: Pages.Basket }">
+							<base-icon :icon="Icons24ShoppingCart" />
+						</nuxt-link>
+						<div v-show="showInfo" class="header__cart-info">
+							<client-only
+								><span>{{ length }}</span></client-only
+							>
+						</div>
 					</div>
-				</div>
-				<div class="header__burger">
-					<base-icon :icon="Icons24Menu"></base-icon>
+					<button class="header__burger" @click="openModal">
+						<base-icon :icon="Icons24Menu"></base-icon>
+					</button>
 				</div>
 			</div>
-		</div>
-	</header>
+		</header>
+		<modal-root v-model="show" class="modal-mobile-menu" :transform="transform">
+			<mobile-menu :show-info="showInfo" :length="length" @close="close"></mobile-menu>
+		</modal-root>
+		<!-- <mobile-menu v-model="show" @update:model-value="closeModal" /> -->
+	</div>
 </template>
 <script lang="ts" setup>
+import { Checkbox, FieldPhone, FieldText, ModalRoot } from '@ui';
 import type { PropType } from 'vue';
 
 import { Icons24Menu, Icons24ShoppingCart } from '#icons';
-import { computed, ref } from '#imports';
+import { computed, ref, useWindowSize, watch } from '#imports';
 import BaseIcon from '~/components/elements/base-icon.vue';
 import BaseLogo from '~/components/elements/base-logo.vue';
+import MenuItems from '~/components/elements/menu-items';
+import MobileMenu from '~/components/elements/mobile-menu';
 import srOnly from '~/components/utils/srOnly.vue';
 import { useStoreBasket } from '~/store/storeBasket';
 import { Pages } from '~/utils/pages';
 
 const basket = useStoreBasket();
-const menu = ref([
-	{ id: 1, title: 'Вакансии', link: 'vacancy' },
-	{ id: 2, title: 'О Компании', link: 'about-company' },
-	{ id: 3, title: 'Направление деятельности', link: 'directions' },
-	{ id: 4, title: 'Cобственная продукция', link: 'products' },
-	{ id: 5, title: 'Контакты', link: 'contact-form' }
-]);
+// const menu = ref([
+// 	{ id: 1, title: 'Вакансии', link: 'vacancy' },
+// 	{ id: 2, title: 'О Компании', link: 'about-company' },
+// 	{ id: 3, title: 'Направление деятельности', link: 'directions' },
+// 	{ id: 4, title: 'Cобственная продукция', link: 'products' },
+// 	{ id: 5, title: 'Контакты', link: 'contact-form' }
+// ]);
 const showInfo = computed(() => {
 	if (Object.keys(basket.items).length) {
 		return true;
 	}
 	return false;
 });
+const { width } = useWindowSize();
 const length = computed(() => {
 	return Object.keys(basket.items).length;
+});
+const show = ref<boolean>(false);
+const transform = {
+	opacity: [0, 1],
+	transform: ['translateX(180px)', 'translateY(0px)']
+};
+
+function close() {
+	console.log(show.value);
+	show.value = false;
+}
+function openModal() {
+	show.value = true;
+}
+watch(width, (value) => {
+	if (value >= 1200) {
+		show.value = false;
+	}
 });
 </script>
 <style scoped lang="scss">
@@ -83,6 +113,14 @@ const length = computed(() => {
 		top: #{utility.rem(0)};
 		border-radius: 0 0 #{utility.rem(16)} #{utility.rem(16)};
 	}
+	&__items-wrapper {
+		gap: #{utility.rem(16)};
+		:deep(.header__menu-item) {
+			@include breakpoints.media-down('xl') {
+				display: none;
+			}
+		}
+	}
 	&__logo-wrapper {
 		svg {
 			@include breakpoints.media-down('xl') {
@@ -100,6 +138,7 @@ const length = computed(() => {
 	}
 	&__burger {
 		display: none;
+		border: none;
 		@include breakpoints.media-down('xl') {
 			display: flex;
 			// height: 48px;
@@ -117,6 +156,12 @@ const length = computed(() => {
 	}
 	&__items-wrapper {
 		display: flex;
+		@include breakpoints.media-down('xl') {
+			display: flex;
+			flex-direction: column;
+			// position: absolute;
+			// top: 0;
+		}
 	}
 
 	&__logo-wrapper {
@@ -140,7 +185,7 @@ const length = computed(() => {
 		line-height: 135%;
 		letter-spacing: #{utility.rem(-0.54)};
 		@include breakpoints.media-down('xl') {
-			display: none;
+			display: flex;
 		}
 		a {
 			text-decoration: none;
@@ -184,7 +229,7 @@ const length = computed(() => {
 		color: #fff;
 	}
 	&__cart:hover {
-		background-color: #1D2939;
+		background-color: #1d2939;
 	}
 	&__cart-info {
 		position: absolute;
@@ -214,5 +259,38 @@ const length = computed(() => {
 			font-size: #{utility.rem(14)};
 		}
 	}
+}
+:deep(.header__menu-item) {
+	padding: #{utility.rem(16)} 0 #{utility.rem(16)};
+	color: #1d2939;
+	font-weight: 500;
+	font-style: normal;
+	font-size: #{utility.rem(18)};
+	line-height: 135%;
+	letter-spacing: #{utility.rem(-0.54)};
+	font-size: #{utility.rem(20)};
+	font-style: normal;
+	font-weight: 600;
+	line-height: 115%;
+	letter-spacing: #{utility.rem(-0.8)};
+	a {
+		text-decoration: none;
+	}
+}
+.header__menu-item:hover a {
+	text-decoration: underline;
+}
+:deep(.mobile__menu-item) {
+	display: none;
+	@include breakpoints.media-down('xl') {
+		display: block;
+	}
+}
+</style>
+
+<style lang="scss">
+.modal-mobile-menu.modal-root .modal-root__wrapper {
+	display: unset;
+	background-color: #eaecf0;
 }
 </style>
