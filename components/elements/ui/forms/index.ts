f890a84f58335a94e676de46f49baa5fafc9c0d5
vadimedu <paketbananov@MacBook-Pro-Paket.local>
2023-12-18 11:34:35 +0500

    feat:index forms add new export field textarea form
    add new export field textarea form

--- components/elements/ui/forms/index.ts
+++ components/elements/ui/forms/index.ts
@@ -1,3 +1,4 @@
 export { default as Checkbox } from './checkbox.vue';
 export { default as FieldPhone } from './field-phone.vue';
 export { default as FieldText } from './field-text.vue';
+export { default as FieldTextarea } from './field-textarea.vue';
